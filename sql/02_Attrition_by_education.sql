97c33722829cef8521bb486ea03e062cb0289833
Sibin Augustine <70462911+Sibin-Augustine@users.noreply.github.com>
2026-09-04 12:59:45 +0530

    Delete sql directory

--- sql/02_Attrition_by_education.sql
+++
@@ -1,7 +0,0 @@
-SELECT EducationField,
-       CAST(SUM(CAST(Attrition AS int))*100.0/
-              (SELECT SUM(CAST(Attrition AS Int))
-               FROM HR_Analytics)AS DECIMAL(5, 0)) AS Attrition_Percentage
-FROM HR_Analytics
-GROUP BY EducationField
-ORDER BY Attrition_Percentage DESC;
